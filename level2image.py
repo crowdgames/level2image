5278a638f923e2e8b32a39d57afbfca4a9601077
Seth Cooper <seth.cooper@gmail.com>
2022-01-10 16:02:11 -0500

    Add support for misc edges.

--- level2image.py
+++ level2image.py
@@ -1,9 +1,9 @@
 import argparse, base64, json, math, os, pathlib, sys
 
-PATH_EDGES_NONE     = 'none'
-PATH_EDGES_LINE     = 'line'
-PATH_EDGES_ARC      = 'arc'
-PATH_EDGES_LIST     = [PATH_EDGES_NONE, PATH_EDGES_LINE, PATH_EDGES_ARC]
+EDGES_NONE     = 'none'
+EDGES_LINE     = 'line'
+EDGES_ARC      = 'arc'
+EDGES_LIST     = [EDGES_NONE, EDGES_LINE, EDGES_ARC]
 
 PATH_TILES_NONE     = 'none'
 PATH_TILES_SQUARE   = 'square'
@@ -21,10 +21,12 @@ parser.add_argument('--gridsize', type=int, help='Grid size.', default=10)
 parser.add_argument('--cfgfile', type=str, help='Config file.', default='cfg-default.json')
 parser.add_argument('--fmt', type=str, choices=FMT_LIST, help='Output format, from: ' + ','.join(FMT_LIST) + '.', default=FMT_PDF)
 parser.add_argument('--stdout', action='store_true', help='Write to stdout instead of file.')
-parser.add_argument('--path-edges', type=str, choices=PATH_EDGES_LIST, help='How to display path edges, from: ' + ','.join(PATH_EDGES_LIST) + '.', default=PATH_EDGES_LINE)
-parser.add_argument('--path-edges-no-arrows', action='store_true', help='Don\'t show arrows on path.')
+parser.add_argument('--path-edges', type=str, choices=EDGES_LIST, help='How to display path edges, from: ' + ','.join(EDGES_LIST) + '.', default=EDGES_LINE)
+parser.add_argument('--misc-edges', type=str, choices=EDGES_LIST, help='How to display misc edges, from: ' + ','.join(EDGES_LIST) + '.', default=EDGES_LINE)
+parser.add_argument('--edges-no-arrows', action='store_true', help='Don\'t show arrows on edges.')
 parser.add_argument('--path-tiles', type=str, choices=PATH_TILES_LIST, help='How to display path tiles, from: ' + ','.join(PATH_TILES_LIST) + '.', default=PATH_TILES_SQUARE)
 parser.add_argument('--path-color', type=str, help='Path color.', default='orangered')
+parser.add_argument('--misc-color', type=str, help='Misc color.', default='darkgoldenrod')
 parser.add_argument('--no-background', action='store_true', help='Don\'t use background images if present.')
 args = parser.parse_args()
 
@@ -34,6 +36,72 @@ if args.stdout and args.fmt != FMT_SVG:
 with open(args.cfgfile, 'rt') as cfgfile:
     cfg = json.load(cfgfile)
 
+
+def distance(ra, ca, rb, cb):
+    return ((ra - rb)**2 + (ca - cb)**2)**0.5
+
+def is_between(ra, ca, rb, cb, rc, cc):
+    if (ra, ca) == (rb, cb) or (rc, cc) == (rb, cb):
+        return False
+    return abs(distance(ra, ca, rb, cb) + distance(rb, cb, rc, cc) - distance(ra, ca, rc, cc)) < 0.01
+
+def svg_line(r1, c1, r2, c2, color, require_arc, arc_avoid_edges, from_circle, to_circle, to_arrow):
+    x1 = (c1 + 0.5) * args.gridsize
+    y1 = (r1 + 0.5) * args.gridsize
+    x2 = (c2 + 0.5) * args.gridsize
+    y2 = (r2 + 0.5) * args.gridsize
+
+    if x1 < x2:
+        orthx = (y2 - y1) / 4
+        orthy = (x1 - x2) / 4
+    else:
+        orthx = (y1 - y2) / 4
+        orthy = (x2 - x1) / 4
+    orthmax = 0.75 * args.gridsize
+    orthlen = distance(0, 0, orthx, orthy)
+    orthx = orthx / orthlen * orthmax
+    orthy = orthy / orthlen * orthmax
+    midx = (x1 + x2) / 2
+    midy = (y1 + y2) / 2
+    curvex = midx + orthx
+    curvey = midy + orthy
+
+    ret = ''
+    if from_circle:
+        ret += '  <circle cx="%.2f" cy="%.2f" r="2" stroke="none" fill="%s"/>\n' % (x1, y1, color)
+    if to_circle:
+        ret += '  <circle cx="%.2f" cy="%.2f" r="2" stroke="none" fill="%s"/>\n' % (x2, y2, color)
+
+    as_arc = require_arc
+    if not as_arc and arc_avoid_edges:
+        for (rj1, cj1, rj2, cj2) in arc_avoid_edges:
+            if is_between(r1, c1, rj1, cj1, r2, c2):
+                as_arc = True
+                break
+            if is_between(r1, c1, rj2, cj2, r2, c2):
+                as_arc = True
+                break
+            if (r1, c1, r2, c2) == (rj2, cj2, rj1, cj1):
+                if (r1, c1, r2, c2) < (rj1, cj1, rj2, cj2):
+                    as_arc = True
+                    break
+
+    if to_arrow:
+        if as_arc:
+            adjust = distance(x1, y1, x2, y2)
+            adjust = max(0.0, min(1.0, (adjust - 10) / (50 - 10))) * 0.4 + 0.6
+            rotate = math.degrees(math.atan2(y2 - (midy + adjust * orthy), x2 - (midx + adjust * orthx)))
+        else:
+            rotate = math.degrees(math.atan2(y2 - y1, x2 - x1))
+        ret += '  <g transform="translate(%.2f %.2f) rotate(%.2f)"><polygon points="0 0, -4 -2, -4 2" stroke="none" fill="%s"/></g>\n' % (x2, y2, rotate, color)
+
+    if as_arc:
+        ret += '  <path d="M %.2f %.2f Q %.2f %.2f %.2f %.2f" stroke="%s" stroke-width="1" stroke-linecap="round" fill="none"/>\n' % (x1, y1, curvex, curvey, x2, y2, color)
+    else:
+        ret += '  <line x1="%.2f" y1="%.2f" x2="%.2f" y2="%.2f" stroke="%s" stroke-width="1" stroke-linecap="round"/>\n' % (x1, y1, x2, y2, color)
+
+    return ret
+
 for levelfile in args.levelfiles:
     print('processing', levelfile)
 
@@ -41,6 +109,7 @@ for levelfile in args.levelfiles:
     max_line_len = 0
     path_edges = None
     path_tiles = None
+    misc_edges = None
 
     with open(levelfile, 'rt') as lvl:
         for line in lvl:
@@ -58,6 +127,12 @@ for levelfile in args.levelfiles:
                 if path_tiles != None:
                     raise RuntimeError('multiple path tiles found')
                 path_tiles = [tuple([int(el) for el in pt.strip().split()]) for pt in line[16:].split(';')]
+                continue
+
+            if line.startswith('META MISC EDGES:'):
+                if misc_edges == None:
+                    misc_edges = []
+                misc_edges += [tuple([int(el) for el in pt.strip().split()]) for pt in line[16:].split(';')]
                 continue
 
             if line.startswith('REM') or line.startswith('META'):
@@ -112,68 +187,31 @@ for levelfile in args.levelfiles:
                 svg += '  <rect x="%d" y="%d" width="%d" height="%d" style="stroke:none;fill:%s;fill-opacity:0.3"/>\n' % (x, y - args.gridsize + 1, args.gridsize, args.gridsize, path_color)
             drawn.add((x, y))
 
-    if path_edges != None and args.path_edges != PATH_EDGES_NONE:
-        print(' - adding edges path')
-        
-        def distance(ra, ca, rb, cb):
-            return ((ra - rb)**2 + (ca - cb)**2)**0.5
+    if misc_edges != None and args.misc_edges != EDGES_NONE:
+        print(' - adding edges misc')
 
-        def is_between(ra, ca, rb, cb, rc, cc):
-            return abs(distance(ra, ca, rb, cb) + distance(rb, cb, rc, cc) - distance(ra, ca, rc, cc)) < 0.01
+        edge_color = args.misc_color
+        avoid_edges = misc_edges
+
+        skip_path_edges = set()
+        if path_edges != None and args.path_edges != EDGES_NONE:
+            for (r1, c1), (r2, c2) in zip(path_edges, path_edges[1:]):
+                skip_path_edges.add((r1, c1, r2, c2))
+
+        for r1, c1, r2, c2 in misc_edges:
+            if (r1, c1, r2, c2) in skip_path_edges:
+                continue
+
+            svg += svg_line(r1, c1, r2, c2, edge_color, args.misc_edges == EDGES_ARC, avoid_edges, False, False, not args.edges_no_arrows)
+
+    if path_edges != None and args.path_edges != EDGES_NONE:
+        print(' - adding edges path')
 
         path_color = args.path_color
+        avoid_edges = [(r1, c1, r2, c2) for ((r1, c1), (r2, c2)) in zip(path_edges, path_edges[1:])]
 
         for ii, ((r1, c1), (r2, c2)) in enumerate(zip(path_edges, path_edges[1:])):
-            x1 = (c1 + 0.5) * args.gridsize
-            y1 = (r1 + 0.5) * args.gridsize
-            x2 = (c2 + 0.5) * args.gridsize
-            y2 = (r2 + 0.5) * args.gridsize
-
-            if x1 < x2:
-                orthx = (y2 - y1) / 4
-                orthy = (x1 - x2) / 4
-            else:
-                orthx = (y1 - y2) / 4
-                orthy = (x2 - x1) / 4
-            orthlen = distance(0, 0, orthx, orthy)
-            orthmax = 0.8 * args.gridsize
-            if orthlen > orthmax:
-                orthx = orthx / orthlen * orthmax
-                orthy = orthy / orthlen * orthmax
-            midx = (x1 + x2) / 2
-            midy = (y1 + y2) / 2
-            curvex = midx + orthx
-            curvey = midy + orthy
-
-            if ii == 0:
-                svg += '  <circle cx="%.2f" cy="%.2f" r="2" stroke="none" fill="%s"/>\n' % (x1, y1, path_color)
-            elif ii + 2 == len(path_edges):
-                svg += '  <circle cx="%.2f" cy="%.2f" r="2" stroke="none" fill="%s"/>\n' % (x2, y2, path_color)
-
-            as_arc = (args.path_edges == PATH_EDGES_ARC)
-
-            if not as_arc:
-                for jj, (rj, cj) in enumerate(path_edges):
-                    if jj == ii or jj == ii + 1:
-                        continue
-
-                    if is_between(r1, c1, rj, cj, r2, c2):
-                        as_arc = True
-                        break
-
-            if not args.path_edges_no_arrows:
-                if as_arc:
-                    adjust = distance(x1, y1, x2, y2)
-                    adjust = max(0.0, min(1.0, (adjust - 10) / (50 - 10))) * 0.4 + 0.6
-                    rotate = math.degrees(math.atan2(y2 - (midy + adjust * orthy), x2 - (midx + adjust * orthx)))
-                else:
-                    rotate = math.degrees(math.atan2(y2 - y1, x2 - x1))
-                svg += '  <g transform="translate(%.2f %.2f) rotate(%.2f)"><polygon points="0 0, -4 -2, -4 2" stroke="none" fill="%s"/></g>\n' % (x2, y2, rotate, path_color)
-
-            if as_arc:
-                svg += '  <path d="M %.2f %.2f Q %.2f %.2f %.2f %.2f" stroke="%s" stroke-width="1" stroke-linecap="round" fill="none"/>\n' % (x1, y1, curvex, curvey, x2, y2, path_color)
-            else:
-                svg += '  <line x1="%.2f" y1="%.2f" x2="%.2f" y2="%.2f" stroke="%s" stroke-width="1" stroke-linecap="round"/>\n' % (x1, y1, x2, y2, path_color)
+            svg += svg_line(r1, c1, r2, c2, path_color, args.path_edges == EDGES_ARC, avoid_edges, ii == 0, ii + 2 == len(path_edges), not args.edges_no_arrows)
 
     svg += '</svg>\n'
 
